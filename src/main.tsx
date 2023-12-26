421a387f237c9a331c5e662e7c5f99bc6c0aaa0e
Sonam N Sherpa <sonamsherpa16@gmail.com>
2023-12-26 16:00:54 -0500

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import Home from "./pages/home/home";
 
-import './global.css'
-import Home from './pages/home/home'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
-)
+);
